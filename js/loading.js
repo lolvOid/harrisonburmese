9b15af997e7ab65e8e597337297469bd0d454280
kiyokata <myatkaungkhant21@gmail.com>
2021-06-11 16:02:15 +0630

    separated loading code from main.js

---
+++ js/loading.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function() {
+  var params = {
+    container: document.getElementById('lottie'),
+    renderer: 'svg',
+    loop:false,
+    autoplay: false,
+    path:"/js/HarrisonLoading2.json",
+    
+  };
+  var anim;
+  anim = lottie.loadAnimation(params);
+    // lottie.stop();
+  
+  lottie.play();
+  setTimeout(() => {
+    $('#loading').fadeOut("normal",function(){
+        $('body').removeClass('position-fixed');
+        $('#loading').remove();
+    })
+  }, 3800)  
+  start() 
+});
+
+function start() {
+  var duration = 3200; // it should finish in 5 seconds !
+  var st = window.performance.now();
+  var val=0;
+  window.requestAnimationFrame(function step(time) {
+    var diff = Math.round(time - st),
+      val = Math.round(diff / duration * 100);
+    val = val > 100 ? 100 : val;
+    // $("#box").css("width", val + "px");
+    $("#box").text(val + "%");
+    if (diff < duration) {
+      window.requestAnimationFrame(step);
+    }
+  })
+}
+
