01da42911543a1fe4c9d5ca80ecd4dff79d6de87
kiyokata <myatkaungkhant21@gmail.com>
2021-06-11 10:17:27 +0630

    plugins minified

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
-// TweenMax.from(".hover-img", 1, {
-//   y: '100',
-//   opacity: 0,
-//   ease: Expo.easeInOut
-//   })
-
-
-// let rule = CSSRulePlugin.getRule(".cssRule:hover::after")
-// let bottle = document.querySelector('.bottle-item-1')
-// bottle.addEventListener('mouseover', (e) => {
-//   let target = e.target.closest('div')
-//   if (!target) {return}
-
-
-
-//   TweenMax.to(".hover-img", 1, {
-//     y: '-100',
-//     opacity: 1,
-//     ease: Expo.easeInOut
-//     })
-// })
-        
-        var params = {
+       var params = {
           container: document.getElementById('lottie'),
           renderer: 'svg',
           loop:false,
@@ -28,43 +6,9 @@
           path:"/js/HarrisonLoading2.json",
           
       };
-
       var anim;
-
       anim = lottie.loadAnimation(params);
       lottie.stop();
-// $('body').waitForImages(function() {
-//   lottie.play();
-//   setTimeout(() => {
-//     $('#loading').fadeOut("normal",function(){
-//         $('body').removeClass('position-fixed');
-//         $('#loading').remove();
-//     })
-//   }, 3500)  
-//   start() 
-// });
-
-// $("body").waitForImages(function(){
-
-//   $("#loader").attr("src", "img/bottleloading.gif");
-//   setTimeout(() => {
-//     $('#loading').fadeOut("normal",function(){
-//         $('body').removeClass('position-fixed');
-//         $('#loading').remove();
-//     })
-//   }, 3500)  
-//   start() 
-  
-// });
-
-// paceOptions = {
-//   ajax: false, // disabled
-//   document: true, // enabled
-//   eventLag: false, // disabled
-//   elements: {
-//     selectors: ['.my-page']
-//   }
-// };
 var frame=0;
 
 $(document).ready(function() {
@@ -94,32 +38,6 @@ function start() {
   })
 }
 
-
-  // Pace.on('done', function() {
-  //   // lottie.play();
-  //   $("#loader").attr("src", "img/bottleloading.gif");
-  //   setTimeout(() => {
-  //     $('#loading').fadeOut("normal",function(){
-  //         $('body').removeClass('position-fixed');
-  //         $('#loading').remove();
-  //     })
-  //   }, 3200)     
-  //   // Pace.stop();
-  // });
-  
- 
-// });
-
-
-// TweenMax.from("#main-title", 1, {
-//   delay: 1,
-//   scale:1,
-//   opacity: 0,
-//   ease: Expo.easeInOut
-//   })
-// $(document).ready(function(){
- 
-// })
 
 $('.slider-bottles').slick({
   autoplay:true,
